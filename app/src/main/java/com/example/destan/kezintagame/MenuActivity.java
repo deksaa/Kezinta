1635074842fa5df981618bbe409fbe6fe34797e2
Destan Erik <erikdestan@gmail.com>
2017-05-24 08:41:54 +0300

    Png files is changed with new ones.RateButton is added.Menu is designed again and tested.

--- app/src/main/java/com/example/destan/kezintagame/MenuActivity.java
+++ app/src/main/java/com/example/destan/kezintagame/MenuActivity.java
@@ -1,13 +1,8 @@
 package com.example.destan.kezintagame;
 
-
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,25 +10,19 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.Interpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class MenuActivity extends FragmentActivity implements
         GoogleApiClient.ConnectionCallbacks,
@@ -45,7 +34,7 @@ public class MenuActivity extends FragmentActivity implements
 
     AlphaAnimation alphaAnimation;
 
-    ImageView playButton, noAdsButton, shareButton, rankButton, logo;
+    ImageView playButton, noAdsButton, shareButton, rankButton, rateButton, logo;
 
     RotateAnimation rotateRight;
     RotateAnimation rotateLeft;
@@ -65,12 +54,14 @@ public class MenuActivity extends FragmentActivity implements
         noAdsButton = (ImageView) findViewById(R.id.noAdsButton);
         shareButton = (ImageView) findViewById(R.id.shareButton);
         rankButton = (ImageView) findViewById(R.id.rankButton);
+        rateButton = (ImageView) findViewById(R.id.rateButton);
 
         imageViews.add(logo);
         imageViews.add(playButton);
-        imageViews.add(noAdsButton);
         imageViews.add(shareButton);
         imageViews.add(rankButton);
+        imageViews.add(rateButton);
+        imageViews.add(noAdsButton);
 
         logo.setVisibility(View.INVISIBLE);
         playButton.setVisibility(View.INVISIBLE);
@@ -206,6 +197,30 @@ public class MenuActivity extends FragmentActivity implements
                     }
                 }
 
+                return true;
+            }
+        });
+
+        rateButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        ImageView view = (ImageView) v;
+                        //overlay is black with transparency of 0x77 (119)
+                        view.getDrawable().setColorFilter(0xFFFFFFFF, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL: {
+                        ImageView view = (ImageView) v;
+                        //clear the overlay
+                        view.getDrawable().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
                 return true;
             }
         });
